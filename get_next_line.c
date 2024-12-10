6a0da8c40352d4a88a01878ef2e170a87efe9072
Hamza Eljary <heljary@c3r9p6.1337.ma>
2024-12-10 11:36:19 +0100

    get_next_line

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: heljary <heljary@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 12:03:35 by heljary           #+#    #+#             */
-/*   Updated: 2024/12/10 10:46:02 by heljary          ###   ########.fr       */
+/*   Updated: 2024/12/10 11:35:53 by heljary          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,21 @@ static char	*rd_line(int fd, char *str, char *buffer)
 {
 	ssize_t	nbytes;
 	char	*temp;
-	
-	nbytes = read(fd, buffer, BUFFER_SIZE);//2 
+
+	nbytes = read(fd, buffer, BUFFER_SIZE);
 	while (nbytes > 0)
 	{
 		buffer[nbytes] = '\0';
 		if (!str)
 			str = ft_strdup("");
-		temp = ft_strjoin(str, buffer);//temp =hall
+		temp = ft_strjoin(str, buffer);
 		free(str);
 		if (!temp)
 			return (NULL);
 		str = temp;
 		if (ft_strchr(str, '\n'))
 			break ;
-		nbytes = read(fd, buffer, BUFFER_SIZE);//2
+		nbytes = read(fd, buffer, BUFFER_SIZE);
 	}
 	if (nbytes < 0)
 	{
@@ -48,7 +48,7 @@ static char	*find_newline(char *line)
 
 	i = 0;
 	while (line[i] != '\n' && line[i] != '\0')
-		i++; //5
+		i++;
 	if (line[i] == '\0')
 		return (NULL);
 	remainder = ft_strdup(line + i + 1);
@@ -59,17 +59,16 @@ static char	*find_newline(char *line)
 	}
 	line[i + 1] = '\0';
 	return (remainder);
-	
 }
 
 char	*get_next_line(int fd)
 {
-	static char		*str;
-	char			*buffer;
-	char			*line;
+	static char	*str;
+	char		*buffer;
+	char		*line;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
-		return (NULL); 
+		return (NULL);
 	buffer = (char *)malloc((size_t)BUFFER_SIZE + 1);
 	if (!buffer)
 		return (NULL);
